609688dddcaf4b25c0e38b7691b91c5974fdfdab
Shivangi10-10 <107495159+Shivangi10-10@users.noreply.github.com>
2023-11-10 12:53:28 +0530

    Update item_ui_design_widget.dart

--- item_ui_design_widget.dart
+++ item_ui_design_widget.dart
@@ -5,17 +5,17 @@ import 'package:shivangi/items.dart';
 
 class ItemUIDesignWidget extends StatefulWidget
 {
-  Items? itemsInfo;
-  BuildContext? context;
+ Items? itemsInfo;
+ BuildContext? context;
 
 
-  ItemUIDesignWidget({
-    this.itemsInfo,
-    this.context,});
+ ItemUIDesignWidget({
+   this.itemsInfo,
+   this.context,});
 
 
-  @override
-  State<ItemUIDesignWidget> createState() => _ItemUIDesignWidgetState();
+ @override
+ State<ItemUIDesignWidget> createState() => _ItemUIDesignWidgetState();
 }
 
 
@@ -24,240 +24,240 @@ class ItemUIDesignWidget extends StatefulWidget
 
 
 class _ItemUIDesignWidgetState extends State<ItemUIDesignWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: ()
-      {
-        //send user to the items details screen
-        Navigator.push(context, MaterialPageRoute(builder: (c)=>ItemDetailsScreen(
-          clickedItemInfo: widget.itemsInfo,
-        )));
-      },
-      splashColor: Colors.purple,
-      child: Padding(
-        padding: const EdgeInsets.all(6.0),
-        child: SizedBox(
-          height: 140,
-          width: MediaQuery.of(context).size.width,
-          child: Row(
+ @override
+ Widget build(BuildContext context) {
+   return InkWell(
+     onTap: ()
+     {
+       //send user to the items details screen
+       Navigator.push(context, MaterialPageRoute(builder: (c)=>ItemDetailsScreen(
+         clickedItemInfo: widget.itemsInfo,
+       )));
+     },
+     splashColor: Colors.purple,
+     child: Padding(
+       padding: const EdgeInsets.all(6.0),
+       child: SizedBox(
+         height: 140,
+         width: MediaQuery.of(context).size.width,
+         child: Row(
 
 
-            children: [
+           children: [
 
 
-              //image
-              Image.network(
-                widget.itemsInfo!.itemImage.toString(),
-                width: 140,
-                height: 140,
-              ),
+             //image
+             Image.network(
+               widget.itemsInfo!.itemImage.toString(),
+               width: 140,
+               height: 140,
+             ),
 
 
-              const SizedBox(width: 4.0,),
+             const SizedBox(width: 4.0,),
 
 
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 15),
+             Expanded(
+               child: Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                   const SizedBox(height: 15),
 
 
-                    //item name
-                    Expanded(
-                      child: Text(
-                        widget.itemsInfo!.itemName.toString(),
-                        maxLines: 2,
-                        style: const TextStyle(
-                          color: Colors.white70,
-                          fontSize: 16,
+                   //item name
+                   Expanded(
+                     child: Text(
+                       widget.itemsInfo!.itemName.toString(),
+                       maxLines: 2,
+                       style: const TextStyle(
+                         color: Colors.white70,
+                         fontSize: 16,
 
 
-                        ),
-                      ),
-                    ),
+                       ),
+                     ),
+                   ),
 
 
-                    const SizedBox(
-                      height: 5.0,
-                    ),
+                   const SizedBox(
+                     height: 5.0,
+                   ),
 
 
-                    //seller name
-                    Expanded(
-                      child: Text(
-                        widget.itemsInfo!.sellerName.toString(),
-                        maxLines: 2,
-                        style: const TextStyle(
-                          color: Colors.pinkAccent,
-                          fontSize: 14,
+                   //seller name
+                   Expanded(
+                     child: Text(
+                       widget.itemsInfo!.sellerName.toString(),
+                       maxLines: 2,
+                       style: const TextStyle(
+                         color: Colors.pinkAccent,
+                         fontSize: 14,
 
 
-                        ),
-                      ),
-                    ),
+                       ),
+                     ),
+                   ),
 
 
-                    const SizedBox(
-                      height: 20.0,
-                    ),
+                   const SizedBox(
+                     height: 20.0,
+                   ),
 
 
-                    //show discount badge
-                    //price after discount
-                    Row(
-                      children: [
-                        //50% OFF badge
-                        Container(
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.rectangle,
-                            color: Colors.pink,
-                          ),
+                   //show discount badge
+                   //price after discount
+                   Row(
+                     children: [
+                       //50% OFF badge
+                       Container(
+                         decoration: const BoxDecoration(
+                           shape: BoxShape.rectangle,
+                           color: Colors.pink,
+                         ),
 
 
-                          alignment: Alignment.topLeft,
-                          width: 40,
-                          height: 44,
-                          child: Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
+                         alignment: Alignment.topLeft,
+                         width: 40,
+                         height: 44,
+                         child: Center(
+                           child: Column(
+                             mainAxisAlignment: MainAxisAlignment.center,
+                             children: const [
 
 
 
 
-                                Text(
-                                  "50%",
-                                  style: TextStyle(
-                                    fontSize: 15.0,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.normal,
-                                  ),
-                                ),
+                               Text(
+                                 "50%",
+                                 style: TextStyle(
+                                   fontSize: 15.0,
+                                   color: Colors.white,
+                                   fontWeight: FontWeight.normal,
+                                 ),
+                               ),
 
 
 
 
-                                Text(
-                                  "OFF",
-                                  style: TextStyle(
-                                    fontSize: 12.0,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.normal,
-                                  ),
-                                ),
+                               Text(
+                                 "OFF",
+                                 style: TextStyle(
+                                   fontSize: 12.0,
+                                   color: Colors.white,
+                                   fontWeight: FontWeight.normal,
+                                 ),
+                               ),
 
 
-                              ],
-                            ),
-                          ),
+                             ],
+                           ),
+                         ),
 
 
-                        ),
+                       ),
 
 
-                        const SizedBox(
-                          width: 10,
-                        ),
+                       const SizedBox(
+                         width: 10,
+                       ),
 
 
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
+                       Column(
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                         children: [
 
 
-                            //original price
+                           //original price
 
 
-                            Row(
-                              children: [
-                                const Text(
-                                  "Original Price: Rs",
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.grey,
-                                    decoration: TextDecoration.lineThrough,
-                                  ),
-                                ),
+                           Row(
+                             children: [
+                               const Text(
+                                 "Original Price: Rs",
+                                 style: TextStyle(
+                                   fontSize: 14,
+                                   color: Colors.grey,
+                                   decoration: TextDecoration.lineThrough,
+                                 ),
+                               ),
 
 
-                                Text(
-                                  (double.parse(widget.itemsInfo!.itemPrice!) + double.parse(widget.itemsInfo!.itemPrice!)).toString(),
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.grey,
-                                    decoration: TextDecoration.lineThrough,
-                                  ),
-                                ),
-                              ],
-                            ),
+                               Text(
+                                 (double.parse(widget.itemsInfo!.itemPrice!) + double.parse(widget.itemsInfo!.itemPrice!)).toString(),
+                                 style: const TextStyle(
+                                   fontSize: 16,
+                                   color: Colors.grey,
+                                   decoration: TextDecoration.lineThrough,
+                                 ),
+                               ),
+                             ],
+                           ),
 
 
-                            //new Price
-                            Row(
-                              children: [
-                                const Text(
-                                  "New Price: ",
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.grey,
-                                  ),
-                                ),
+                           //new Price
+                           Row(
+                             children: [
+                               const Text(
+                                 "New Price: ",
+                                 style: TextStyle(
+                                   fontSize: 14,
+                                   color: Colors.grey,
+                                 ),
+                               ),
 
 
-                                const Text(
-                                  "Rs",
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.red,
-                                  ),
-                                ),
+                               const Text(
+                                 "Rs",
+                                 style: TextStyle(
+                                   fontSize: 16,
+                                   color: Colors.red,
+                                 ),
+                               ),
 
 
-                                Text(
-                                  widget.itemsInfo!.itemPrice.toString(),
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.red,
-                                  ),
-                                ),
-                              ],
-                            ),
+                               Text(
+                                 widget.itemsInfo!.itemPrice.toString(),
+                                 style: const TextStyle(
+                                   fontSize: 16,
+                                   color: Colors.red,
+                                 ),
+                               ),
+                             ],
+                           ),
 
 
 
 
-                          ],
-                        ),
-                      ],
-                    ),
+                         ],
+                       ),
+                     ],
+                   ),
 
 
-                    const SizedBox(
-                      height: 8.0,
-                    ),
+                   const SizedBox(
+                     height: 8.0,
+                   ),
 
 
 
 
-                    const Divider(
-                      height: 4,
-                      color: Colors.white70,
-                    ),
+                   const Divider(
+                     height: 4,
+                     color: Colors.white70,
+                   ),
 
 
 
 
-                  ],
-                ),
-              ),
-            ],
+                 ],
+               ),
+             ),
+           ],
 
 
-          ),
-        ),
-      ),
-    );
-  }
+         ),
+       ),
+     ),
+   );
+ }
 }
