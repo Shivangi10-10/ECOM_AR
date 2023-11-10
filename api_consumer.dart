34d11c56cab8c9bc2e23dbcc4daf007af82aa36e
Shivangi10-10 <107495159+Shivangi10-10@users.noreply.github.com>
2023-11-10 12:50:45 +0530

    Update api_consumer.dart

--- api_consumer.dart
+++ api_consumer.dart
@@ -1,3 +1,4 @@
+
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 import 'package:shivangi/global.dart';
@@ -5,51 +6,51 @@ import 'package:shivangi/global.dart';
 
 class ApiConsumer
 {
-  Future<Uint8List> removeImageBackgroundApi(String imagePath) async
-  {
-    //request
-    var requestApi = http.MultipartRequest(
-        "POST",
-        Uri.parse("https://api.remove.bg/v1.0/removebg")
-    );
-    //which img file to send - define
-    requestApi.files.add(
-        await http.MultipartFile.fromPath(
-            "image_file",
-            imagePath
-        )
-    );
+ Future<Uint8List> removeImageBackgroundApi(String imagePath) async
+ {
+   //request
+   var requestApi = http.MultipartRequest(
+     "POST",
+     Uri.parse("https://api.remove.bg/v1.0/removebg")
+   );
+   //which img file to send - define
+   requestApi.files.add(
+     await http.MultipartFile.fromPath(
+       "image_file",
+       imagePath
+     )
+   );
 
 
-    //API header communicate with help of API key
+   //API header communicate with help of API key
 
 
-    requestApi.headers.addAll(
-        {
-          "X-API-Key": apiKeyRemoveImageBackground
+   requestApi.headers.addAll(
+     {
+       "X-API-Key": apiKeyRemoveImageBackground
 
 
-        });
+     });
 
 
-    //send request and receive response
-    final responseFromApi = await requestApi.send();
+   //send request and receive response
+   final responseFromApi = await requestApi.send();
 
 
-    if(responseFromApi.statusCode == 200)
-    {
-      http.Response getTransparentImageFromResponse = await http.Response.fromStream(responseFromApi);
-      return getTransparentImageFromResponse.bodyBytes;
-    }
-    else
-    {
-      throw Exception("Error Occured:: " + responseFromApi.statusCode.toString());
-    }
-
-
-
+   if(responseFromApi.statusCode == 200)
+   {
+     http.Response getTransparentImageFromResponse = await http.Response.fromStream(responseFromApi);
+     return getTransparentImageFromResponse.bodyBytes;
+   }
+   else
+   {
+     throw Exception("Error Occured:: " + responseFromApi.statusCode.toString());
+   }
 
 
 
-  }
+
+
+
+ }
 }
