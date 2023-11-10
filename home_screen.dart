9e99ad3c4350eb3b64ad6c1015146828f50e9814
Shivangi10-10 <107495159+Shivangi10-10@users.noreply.github.com>
2023-11-10 12:51:25 +0530

    Update home_screen.dart

--- home_screen.dart
+++ home_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:shivangi/item_ui_design_widget.dart';
@@ -7,95 +8,95 @@ import 'package:shivangi/items.dart';
 
 class HomeScreen extends StatefulWidget
 {
-  const HomeScreen({super.key});
+ const HomeScreen({super.key});
 
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
+ @override
+ State<HomeScreen> createState() => _HomeScreenState();
 }
 
 
 class _HomeScreenState extends State<HomeScreen>
 {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: const Text(
-          "EcomAR App",
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     backgroundColor: Colors.black,
+     appBar: AppBar(
+       backgroundColor: Colors.black,
+       title: const Text(
+         "EcomAR App",
           style: TextStyle(
             fontSize: 18,
             letterSpacing: 2,
             fontWeight: FontWeight.bold,
           ),
-        ),
-        actions: [
-          IconButton(
-              onPressed: ()
-              {
-                Navigator.push(context, MaterialPageRoute(builder: (c)=>ItemsUploadScreen()));
+       ),
+       actions: [
+         IconButton(
+           onPressed: ()
+           {
+             Navigator.push(context, MaterialPageRoute(builder: (c)=>ItemsUploadScreen()));
 
 
-              },
-              icon: const Icon(
-                  Icons.add,         //this is adding a plus symbol on the top of my homescreen
-                  color: Colors.white
-              )
-          ),
-        ],
-      ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection("items")
-            .orderBy("publishedDate",descending: true)
-            .snapshots(),
+           },
+           icon: const Icon(
+             Icons.add,         //this is adding a plus symbol on the top of my homescreen
+             color: Colors.white
+           )
+         ),
+     ],
+     ),
+     body: StreamBuilder(
+       stream: FirebaseFirestore.instance
+           .collection("items")
+           .orderBy("publishedDate",descending: true)
+           .snapshots(),
 
 
-        builder: (context, AsyncSnapshot dataSnapshot)
-        {
-          if(dataSnapshot.hasData)
-          {
-            return ListView.builder(
-              itemCount: dataSnapshot.data!.docs.length,
-              itemBuilder: (context, index)
-              {
-                Items eachItemInfo = Items.fromJson(
-                    dataSnapshot.data!.docs[index].data() as Map<String, dynamic>
-                );
+       builder: (context, AsyncSnapshot dataSnapshot)
+       {
+         if(dataSnapshot.hasData)
+         {
+           return ListView.builder(
+             itemCount: dataSnapshot.data!.docs.length,
+             itemBuilder: (context, index)
+             {
+               Items eachItemInfo = Items.fromJson(
+                 dataSnapshot.data!.docs[index].data() as Map<String, dynamic>
+               );
 
 
-                return ItemUIDesignWidget(
-                  itemsInfo: eachItemInfo,
-                  context: context,
-                );
-              },
-            );
+               return ItemUIDesignWidget(
+                 itemsInfo: eachItemInfo,
+                 context: context,
+               );
+             },
+           );
 
 
-          }
-          else
-          {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [
-                Center(
-                  child: Text(
-                    "Data is not available. ",
-                    style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.grey,
+         }
+         else
+         {
+           return Column(
+             crossAxisAlignment: CrossAxisAlignment.center,
+             children: const [
+               Center(
+                 child: Text(
+                   "Data is not available. ",
+                   style: TextStyle(
+                     fontSize: 30,
+                     color: Colors.grey,
 
 
-                    ),
-                  ),
-                ),
-              ],
-            );
-          }
-        },
-      ),
-    );
-  }
+                   ),
+                 ),
+               ),
+             ],
+           );
+         }
+       },
+     ),
+   );
+ }
 }
