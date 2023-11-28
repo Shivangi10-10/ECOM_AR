82e6f81da1331933f36af31867cedaf712c70d32
Bhavya-31 <140785122+Bhavya-31@users.noreply.github.com>
2023-11-29 00:03:09 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,44 +1,49 @@
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:shivangi/home_screen.dart';
+import 'package:bhavya/home_screen.dart';
+import 'package:provider/provider.dart';
+import 'cart_model.dart';
 
 
 Future<void> main() async
 {
 
 
- try{
-   WidgetsFlutterBinding.ensureInitialized();
-   await Firebase.initializeApp();
- }
- catch(errorMsg)
- {
-   print("Error:" + errorMsg.toString());
+  try{
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+  }
+  catch(errorMsg)
+  {
+    print("Error:" + errorMsg.toString());
 
 
- }
+  }
 
 
- runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: MyApp(),
+    ),
+  );
 }
 
 
 class MyApp extends StatelessWidget {
- const MyApp({super.key});
+  const MyApp({super.key});
 
 
- @override
- Widget build(BuildContext context) {
-   return MaterialApp(
-     title: 'Flutter EcomAR',
-     theme: ThemeData(
-       primarySwatch: Colors.purple,
-     ),
-     debugShowCheckedModeBanner: false,
-     home: const HomeScreen(),
-   );
- }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter EcomAR',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
 }
-
 
