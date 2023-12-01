42fcf4f66ca65f7794c796867c07219bd5b4da1b
Bhavya-31 <140785122+Bhavya-31@users.noreply.github.com>
2023-12-01 20:53:24 +0530

    Update cart_page.dart

--- cart_page.dart
+++ cart_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shivangi/items.dart';
-import 'package:multi_select_flutter/multi_select_flutter.dart';
+import 'package:bhavya/items.dart';
 import 'package:provider/provider.dart';
 import 'cart_model.dart';
 import 'item_details_screen.dart';
@@ -16,85 +15,59 @@ class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black, // Set the background color to black
       appBar: AppBar(
-        title: const Text(
-          'Cart',
-          style: TextStyle(color: Colors.white), // Set the text color to grey
-        ),
+        title: const Text('Cart'),
       ),
       body: Consumer<CartModel>(
         builder: (context, cartModel, child) {
-          return ListView.builder(
-            itemCount: cartModel.items.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(
-                  cartModel.items[index].itemName ?? 'Unknown name',
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(color: Colors.grey), // Set the text color to grey
-                ),
-                subtitle: Text(
-                  cartModel.items[index].itemDescription ?? 'Unknown name',
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(color: Colors.grey), // Set the text color to grey
-                ),
-                trailing: IconButton(
-                  icon: const Icon(
-                    Icons.remove_shopping_cart,
-                    color: Colors.grey, // Set the icon color to grey
-                  ),
-                  onPressed: () {
-                    cartModel.removeItem(cartModel.items[index]);
+          double totalPrice = cartModel.items.fold<double>(0, (a, b) => a + num.parse(b.itemPrice!));
+
+          return Column(
+            children: <Widget>[
+              Expanded(
+                child: ListView.builder(
+                  itemCount: cartModel.items.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(cartModel.items[index].itemName ?? 'Unknown name', overflow: TextOverflow.ellipsis),
+                      subtitle: Text(cartModel.items[index].itemDescription ?? 'Unknown name', overflow: TextOverflow.ellipsis),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.remove_shopping_cart),
+                        onPressed: () {
+                          cartModel.removeItem(cartModel.items[index]);
+                        },
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ItemDetailsScreen(clickedItemInfo: cartModel.items[index]),
+                          ),
+                        );
+                      },
+                    );
                   },
                 ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ItemDetailsScreen(clickedItemInfo: cartModel.items[index]),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Total: \$${totalPrice.toStringAsFixed(2)}'),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Handle the 'Pay Now' button press
+                      },
+                      child: Text('Pay Now'),
                     ),
-                  );
-                },
-              );
-            },
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        extendedPadding: EdgeInsets.all(8),
-        backgroundColor: Colors.pinkAccent,
-        onPressed: () async {
-          final List<Items> items = Provider.of<CartModel>(context, listen: false).items;
-          final List<Items>? selected = await showDialog<List<Items>>(
-            context: context,
-            builder: (BuildContext context) {
-              return Material(
-                child: MultiSelectDialogField(
-                  items: items.map((item) => MultiSelectItem<Items>(item, item.itemName ?? 'Unknown name')).toList(),
-                  listType: MultiSelectListType.CHIP,
-                  title: Text("Select Items"),
-                  onConfirm: (values) {
-                    selectedItems = values;
-                  },
+                  ],
                 ),
-              );
-            },
+              ),
+            ],
           );
-          if (selected != null) {
-            // Add your logic for trying multiple items virtually here
-          }
         },
-        label: const Text(
-          "Try Multiple",
-          style: TextStyle(color: Colors.white), // Set the text color to grey
-        ),
-        icon: const Icon(
-          Icons.mobile_screen_share_rounded,
-          color: Colors.white,
-        ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
